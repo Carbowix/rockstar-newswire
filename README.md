d7d40340e02fdc73c44a129a3a5300a9eb4194c5
Carbowix <killeryoyo01@gmail.com>
2019-10-22 00:32:16 +0200

    Updated to new version

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-# gtao-newswire
-GTA Online news feed from Rockstar's newswire to your discord server.
+# rockstar-newswire
+Rockstar's [newswire](https://www.rockstargames.com/newswire) feed to your discord server. Supports (Latest, GTA V, Red Dead 2 Redemption)
 
 ## Install
 - Install the required Node packages via `npm i` or `yarn install`
-- Replace `webHookURL` with your discord's [webhook URL](https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks).
-- run it with `node newswire.js`
+
+## API
+
+```js
+    let newswire = require('./newswire');
+    let latestNews = new newswire(type, webhookURL);
+    // Available Types: rdr2, gtav, latest
+    // Webhook URL: https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks
+    // News should automatically post and update every 2 hours.
+```
+
+## Tips
+- You require discord [webhook URL](https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks).
 - Feed refreshes every 2 hours to make sure its up-to-date.
+- It's recommened to take `newsdb.json` with you if you're porting the project to another host to prevent redundant news posts.
 
 ## Demo
 ![](./demo.png "Example of news feed.")
+
+## Credits
+- Rockstar for their [newswire](https://www.rockstargames.com/newswire) JSON support
+- [got](https://www.npmjs.com/package/got) for lightweight simple https requests.
--- newswire.js
+++ newswire.js
@@ -1,41 +1,105 @@
-const webhook = 'webHook URL';
-const news = 'https://www.rockstargames.com/newswire/tags.json?tags=702&page=1';
+const genres = {
+    latest: 'https://www.rockstargames.com/newswire/get-posts.json?page=1',
+    rdr2: 'https://www.rockstargames.com/newswire/tags.json?tags=716&page=1',
+    gtav: 'https://www.rockstargames.com/newswire/tags.json?tags=702&page=1'
+};
+const newsDir = './newswire.json';
 const request = require('got');
-const level = require('level');
 const refreshInterval = 7.2e+6; // 2 hours
-let db = level('newswire');
+const fs = require('fs');
+let articles;
 
-async function getNewArticle() {
-    console.log('[CHECK] Checking for new articles');
-    return processRequest().then(async (res) => {
-        let article = res.posts[0];
-        let check = await checkArticleExists(article.id.toString());
-        if (!check) {
-            let tags = [];
-            await article.primary_tags.map(tag => tags.push(tag.name))
-            addArticle(article.id.toString(), article.link);
-            return {
-                title: article.title,
-                content: article.blurb_short,
-                link: article.link,
-                img: article['preview_images_parsed'].large.src,
-                tags: tags
+fs.readFile(newsDir, 'utf8', (err, jsonString) => {
+    articles = jsonString ? JSON.parse(jsonString) : {};
+});
+
+
+class newswire {
+    constructor(genre, webhook) {
+        if (!genres[genre]) return console.log('Invalid genre. Valid genres: (latest, rdr2, gtav)');
+        this.genre = genre;
+        this.genreFeed = genres[genre];
+        this.webhook = webhook;
+        this.main();
+    }
+
+    async main() {
+        console.log('[READY] Started news feed for ' + this.genre + '. Feed refreshes every 2 hours.');
+        let article = await this.getNewArticle();
+        if (article) this.sendArticle(article);
+        setInterval(async _ => {
+            console.log('[REFRESH] Refreshing news feed for ' + this.genre);
+            let article = await this.getNewArticle();
+            article && article.title ? this.sendArticle(article) : console.log('[ERROR] Failed to retrieve article for ' + this.genre);
+        }, refreshInterval);
+    }
+
+    sendArticle(article) {
+        article.title = sanitfy(article.title);
+        article.content = sanitfy(article.content);
+        let tags = '' + article.tags.map(tag => '`' + tag + '` ');
+        console.log(`[NEW] ${this.genre}: ${article.title} (${article.link})`);
+        const embed = {
+            'embeds': [{
+                'author': {
+                    'name': 'Newswire',
+                    'url': 'https://www.rockstargames.com/newswire',
+                    'icon_url': 'https://img.icons8.com/color/48/000000/rockstar-games.png'
+                },
+                'title': article.title,
+                'url': article.link,
+                'description': tags,
+                'color': 15258703,
+                'fields': [{
+                    'name': 'Summary',
+                    'value': article.content,
+                    'inline': false
+                }],
+                'image': {
+                    'url': article.img
+                }
+            }]
+        };
+        request.post(webhook, {
+            body: JSON.stringify(embed),
+            headers: {
+                'content-type': 'application/json'
             }
-        } else {
-            return false;
-        }
-    }).catch(console.log);
+        });
+    }
+
+    async getNewArticle() {
+        console.log('[CHECK] Checking for new articles for ' + this.genre);
+        return this.processRequest().then(async (res) => {
+            let article = res.posts[0];
+            let check = checkArticleExists(article.id.toString());
+            if (!check) {
+                let tags = [];
+                await article.primary_tags.map(tag => tags.push(tag.name))
+                addArticle(article.id.toString(), article.link);
+                return {
+                    title: article.title,
+                    content: article.blurb_short,
+                    link: article.link,
+                    img: article['preview_images_parsed'].large.src,
+                    tags: tags
+                }
+            } else {
+                console.log('[CHECK] No new articles found for ' + this.genre)
+                return false;
+            }
+        }).catch(console.log);
+    }
+
+    processRequest() {
+        return new Promise(async (resolve, reject) => {
+            const response = await request(this.genreFeed).catch(reject);
+            resolve(JSON.parse(response.body));
+        });
+    }
 }
 
-function addArticle(article, url) {
-    return db.put(article, url);
-}
-
-function checkArticleExists(article) {
-    return db.get(article);
-}
-
-async function sanitfy(text) {
+function sanitfy(text) {
     text = text
         .replace('<span class=\'subtitle\'>', '(')
         .replace('</span>', ')')
@@ -45,59 +109,21 @@ async function sanitfy(text) {
     return text;
 }
 
-async function sendArticle(article) {
-    article.title = await sanitfy(article.title);
-    article.content = await sanitfy(article.content);
-    let tags = '' + article.tags.map(tag => '`' + tag + '` ');
-    console.log(`[NEW] ${article.title} (${article.link})`);
-    const embed = {
-        'embeds': [{
-            'author': {
-                'name': 'Newswire',
-                'url': 'https://www.rockstargames.com/newswire',
-                'icon_url': 'https://img.icons8.com/color/48/000000/rockstar-games.png'
-            },
-            'title': article.title,
-            'url': article.link,
-            'description': tags,
-            'color': 15258703,
-            'fields': [{
-                'name': 'Summary',
-                'value': article.content,
-                'inline': false
-            }],
-            'image': {
-                'url': article.img
-            }
-        }]
-    };
-    await request.post(webhook, {
-        body: JSON.stringify(embed),
-        headers: {
-            'content-type': 'application/json'
+function addArticle(article, url) {
+    if (articles) {
+        if (!articles[article]) {
+            articles[article] = url;
+            fs.writeFile(newsDir, JSON.stringify(articles, null, 2), (err) => {
+                if (err) console.log('Failed to save articles to db due ' + err);
+            });
+        } else {
+            console.log('Article ID: ' + article + ' already exists in database.');
         }
-    });
+    }
 }
 
-function processRequest() {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await request(news);
-            resolve(JSON.parse(response.body));
-        } catch (error) {
-            reject(error);
-        }
-    })
+function checkArticleExists(articleID) {
+    return (articles && articles[articleID]);
 }
 
-const main = async () => {
-    console.log('[READY] Started news feed');
-    let article = await getNewArticle();
-    if (article) sendArticle(article);
-    setInterval(async _ => {
-        let article = await getNewArticle();
-        if (article) sendArticle(article);
-    }, refreshInterval);
-};
-
-main();
+module.exports = newswire;
