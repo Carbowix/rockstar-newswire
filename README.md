b7fa99c165b7b649ee8b48abc612f20497f2267c
Carbowix <killeryoyo01@gmail.com>
2021-03-20 01:39:42 +0200

    Updated news feed with the latest rockstar API

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # rockstar-newswire
-Rockstar's [newswire](https://www.rockstargames.com/newswire) feed to your discord server. Supports (Latest, GTA V, Red Dead 2 Redemption)
-
-## NOTE: This app is no longer functioning due to changes with the newswire website API. I'll be patching it as soon as I have time.
+Bring Rockstar [newswire](https://www.rockstargames.com/newswire) feed to your discord server.
 
 ## Install
 - Install the required Node packages via `npm i` or `yarn install`
@@ -11,19 +9,32 @@ Rockstar's [newswire](https://www.rockstargames.com/newswire) feed to your disco
 ```js
     let newswire = require('./newswire');
     let latestNews = new newswire(type, webhookURL);
-    // Available Types: rdr2, gtav, latest
+    // Available Types: rdr2, gtav, latest, music, fanart, fanvideos, creator, tips, rockstar, updates,
     // Webhook URL: https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks
     // News should automatically post and update every 2 hours.
 ```
+### Available types
+- `creator` (Creator jobs articles featured by Rockstar)
+- `fanart` (General fans' art articles from any Rockstar game)
+- `fanvideos` (General fans' showoff videos articles from any Rockstar game)
+- `gtav` (GTA V general news)
+- `music` (Music production articles)
+- `latest` (Latest news from any type that shows on newswire homepage)
+- `rdr2` (Red dead redemption 2 general news)
+- `rockstar` (Rockstar company updates)
+- `tips` (General game tips from Rockstar)
+- `updates` (Any Released game updates)
 
-## Tips
+## Notes
 - You require discord [webhook URL](https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks).
-- Feed refreshes every 2 hours to make sure its up-to-date.
+- Feed refreshes every 2 hours to make sure its up-to-date. If you would like to change it then you're required to change this [variable](https://github.com/Carbowix/rockstar-newswire/blob/master/newswire.js#L18) using that time [converter](http://www.unitconversion.org/time/seconds-to-milliseconds-conversion.html). It has to be in **milliseconds** in order to operate properly.
 - It's recommened to take `newsdb.json` with you if you're porting the project to another host to prevent redundant news posts.
-
+- It is not guranteed that it can trace multiple new news posts of the same type since it only traces the last post posted. In-order to avoid such error, you can lower the news feed refresh rate as specified previously. If you have a idea on how to improve it then feel free to contribute.
+- This is a small research project and it's not meant to be used as a network harm tool.
 ## Demo
 ![](./demo.png "Example of news feed.")
 
 ## Credits
-- Rockstar for their [newswire](https://www.rockstargames.com/newswire) JSON support
+- Rockstar [newswire](https://www.rockstargames.com/newswire).
 - [got](https://www.npmjs.com/package/got) for lightweight simple https requests.
+- [puppeteer](https://www.npmjs.com/package/puppeteer) for their virtual browser network tracing.
--- newswire.js
+++ newswire.js
@@ -1,44 +1,54 @@
 const genres = {
-    latest: 'https://www.rockstargames.com/newswire/get-posts.json?page=1',
-    rdr2: 'https://www.rockstargames.com/newswire/tags.json?tags=716&page=1',
-    gtav: 'https://www.rockstargames.com/newswire/tags.json?tags=702&page=1'
+    latest: null,
+    music: 30,
+    rockstar: 43,
+    tips: 121,
+    gtav: 702,
+    updates: 705,
+    fanvideos: 706,
+    fanart: 708,
+    creator: 728,
+    rdr2: 736,
 };
-const newsDir = './newswire.json';
+const puppeteer = require('puppeteer');
 const request = require('got');
-const refreshInterval = 7.2e+6; // 2 hours
 const fs = require('fs');
-let articles;
+const newsDir = './newswire.json';
+const mainLink = 'https://graph.rockstargames.com/';
+const refreshInterval = 7.2e+6; // 2 hours in milliseconds. If you would like to change it (http://www.unitconversion.org/time/seconds-to-milliseconds-conversion.html)
+let articles, newsHash;
 
 fs.readFile(newsDir, 'utf8', (err, jsonString) => {
     articles = jsonString ? JSON.parse(jsonString) : {};
 });
 
-
 class newswire {
     constructor(genre, webhook) {
-        if (!genres[genre]) return console.log('Invalid genre. Valid genres: (latest, rdr2, gtav)');
+        if (typeof genres[genre] == 'undefined') return console.log('Invalid genre. Available genres:' + Object.keys(genres).map(gen => ' ' + gen));
         this.genre = genre;
-        this.genreFeed = genres[genre];
+        this.genreID = genres[genre];
         this.webhook = webhook;
         this.main();
     }
 
     async main() {
+        let article;
         console.log('[READY] Started news feed for ' + this.genre + '. Feed refreshes every 2 hours.');
-        let article = await this.getNewArticle();
-        if (article) this.sendArticle(article);
+        newsHash = await getHashToken();
+        article = await this.getNewArticle();
+        if (!(article instanceof TypeError) && article.title) {
+            this.sendArticle(article)
+        }
         setInterval(async _ => {
             console.log('[REFRESH] Refreshing news feed for ' + this.genre);
-            let article = await this.getNewArticle();
-            article && article.title ? this.sendArticle(article) : console.log('[ERROR] Failed to retrieve article for ' + this.genre);
+            article = await this.getNewArticle();
+            !(article instanceof TypeError) && article.title ? this.sendArticle(article) : console.log(article.message);
         }, refreshInterval);
     }
 
     sendArticle(article) {
-        article.title = sanitfy(article.title);
-        article.content = sanitfy(article.content);
-        let tags = '' + article.tags.map(tag => '`' + tag + '` ');
         console.log(`[NEW] ${this.genre}: ${article.title} (${article.link})`);
+        article.tags = '' + article.tags.map(tag => '`' + tag + '` ');
         const embed = {
             'embeds': [{
                 'author': {
@@ -48,19 +58,19 @@ class newswire {
                 },
                 'title': article.title,
                 'url': article.link,
-                'description': tags,
+                'description': article.tags,
                 'color': 15258703,
-                'fields': [{
-                    'name': 'Summary',
-                    'value': article.content,
-                    'inline': false
-                }],
+                'fields': [],
                 'image': {
                     'url': article.img
+                },
+                'footer': {
+                    "icon_url": "https://img.icons8.com/color/48/000000/rockstar-games.png",
+                    "text": article.date
                 }
             }]
         };
-        request.post(webhook, {
+        request.post(this.webhook, {
             body: JSON.stringify(embed),
             headers: {
                 'content-type': 'application/json'
@@ -69,44 +79,61 @@ class newswire {
     }
 
     async getNewArticle() {
-        console.log('[CHECK] Checking for new articles for ' + this.genre);
+        console.log('[CHECK] Checking for new articles in ' + this.genre);
         return this.processRequest().then(async (res) => {
-            let article = res.posts[0];
-            let check = checkArticleExists(article.id.toString());
+            if (res && res.errors != null) {
+                if (res.data == null && res.errors[0].message == 'PersistedQueryNotFound') {
+                    console.log('[HASH] Token has expired, generating new one.');
+                    newsHash = await getHashToken().catch(console.log);
+                    res = await this.processRequest().catch(console.log);
+                } else {
+                    return new TypeError('[ERROR] Rockstar API couldn\'t retrieve articles.');
+                }
+            }
+            let article = res.data.posts.results[0];
+            let check = await checkArticleExists(article.id);
             if (!check) {
                 let tags = [];
+                article.url = 'https://www.rockstargames.com' + article.url;
                 await article.primary_tags.map(tag => tags.push(tag.name))
-                addArticle(article.id.toString(), article.link);
+                addArticle(article.id.toString(), article.url);
                 return {
                     title: article.title,
-                    content: article.blurb_short,
-                    link: article.link,
-                    img: article['preview_images_parsed'].large.src,
+                    link: article.url,
+                    img: article['preview_images_parsed']['newswire_block']['d16x9'],
+                    date: article.created,
                     tags: tags
                 }
             } else {
-                console.log('[CHECK] No new articles found for ' + this.genre)
-                return false;
+                return new TypeError('[CHECK] No new articles found for ' + this.genre)
             }
         }).catch(console.log);
     }
 
-    processRequest() {
+    async processRequest() {
         return new Promise(async (resolve, reject) => {
-            const response = await request(this.genreFeed).catch(reject);
+            const searchParams = new URLSearchParams([
+                ['operationName', 'NewswireList'],
+                ['variables', JSON.stringify({
+                    page: 1,
+                    tagId: this.genreID,
+                    metaUrl: '/newswire',
+                    locale: 'en_us'
+                })],
+                ['extensions', JSON.stringify({
+                    persistedQuery: {
+                        version: 1,
+                        sha256Hash: newsHash
+                    }
+                })]
+            ]);
+            const response = await request.post(mainLink, {
+                searchParams: searchParams,
+                json: true
+            }).catch(reject);
             resolve(JSON.parse(response.body));
         });
     }
-}
-
-function sanitfy(text) {
-    text = text
-        .replace('<span class=\'subtitle\'>', '(')
-        .replace('</span>', ')')
-        .replace(/<\/?strong[^>]*>/g, '**')
-        .replace(/<\s*a[^>]*>(.*?)<\s*\/s*a>/i, '')
-        .replace('&amp;', '\'')
-    return text;
 }
 
 function addArticle(article, url) {
@@ -121,6 +148,40 @@ function addArticle(article, url) {
         }
     }
 }
+
+async function getHashToken() {
+    return new Promise(async (res, rej) => {
+        try {
+            const browser = await puppeteer.launch({
+                headless: true
+            });
+            const page = await browser.newPage();
+            await page.setRequestInterception(true);
+            page.on('request', interceptedRequest => {
+                if (interceptedRequest.url().includes('operationName=NewswireList')) {
+                    let url = interceptedRequest.url();
+                    let params = url.split('?')[1];
+                    let query = new URLSearchParams(params);
+                    let hash = '';
+                    for (let pair of query.entries()) {
+                        if (pair[0] == 'extensions' && pair[1]) {
+                            hash = JSON.parse(pair[1])['persistedQuery']['sha256Hash'];
+                            interceptedRequest.abort();
+                            browser.close();
+                            console.log(hash);
+                            res(hash);
+                        }
+                    }
+                } else {
+                    interceptedRequest.continue();
+                }
+            });
+            page.goto('https://www.rockstargames.com/newswire');
+        } catch (e) {
+            rej(e.stack);
+        }
+    });
+};
 
 function checkArticleExists(articleID) {
     return (articles && articles[articleID]);
